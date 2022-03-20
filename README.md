8bf04d07584d71bf570ce4767ec23898fe793648
Brianna <briannaodell1437@gmail.com>
2022-03-19 23:05:34 -0700

    Final structure update readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # bootcamp_quiz
+
+This project is to practice HTML, CSS, and Javascript skills for the UW Coding Class.
+
+## Usage
+
+In this first version quiz, a user should be able to 
+- Click the start to begin a short javascript quiz
+- Start a 3-minute timer that will begin counting down
+     - This timer will turn red to warn when there is 15 seconds left remaining
+     - Timer will automatically finish the game if time runs out
+     - Timer will stop when the Finish button is clicked
+- Visualize what answers are true and false
+- Use on a mobile device with better sizing and readability
+
+## Sample Images
+
+![Opening the quiz, displays a start button](/assets/images/Bootcamp-Quiz.png)
+
+![First question of the quiz](/assets/images/quiz-questions.png)
+
+![Mobile display with color indication of true and false answers](/assets/images/mobile-version.png)
+
+![Final page with bug issue](/assets/images/debug-issues.png)
+
+
+### Known Issues
+
+- Previous button doesn't show up until last question
+- Last question doesn't display true/false indicators
+- Wrong answers do not create additional time penalty
+- Score collection is not linked properly
+- List does not display for scores
+- Top left scoreboard does not update
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
-//Game Functions
+//Game Functions - lots of reference from https://www.youtube.com/watch?v=riDzcEQbX6k
 
-//clicking Start
-// var startButton = document.getElementById('start-button')
+
 var startButton = document.querySelector('#start-button')
 var finishButton = document.querySelector('#finish-button')
 var questionBox = document.querySelector('#question')
@@ -10,8 +9,17 @@ var prevButton = document.querySelector('#prevButton')
 var nextButton = document.querySelector('#nextButton')
 var multipleChoice = document.querySelector('#multichoice')
 var scoreForm = document.querySelector('#score-form')
+var scoreHere = document.querySelector('#score-here')
+
 var currentQuestion
 var firstQuestion
+
+function init() {
+  getScore()
+}
+
+
+
 
 startButton.addEventListener('click', startGame)
 finishButton.addEventListener('click', finishGame)
@@ -24,6 +32,7 @@ prevButton.addEventListener("click", function() {
   showFirstQuestion();
 })
 
+//clicking Start pulls questions, removes start button, shows rest of quiz, starts timer
 function startGame() {
   console.log('Started')
   currentQuestion = questions.sort()
@@ -34,12 +43,15 @@ function startGame() {
   nextButton.classList.remove('hide')
   multipleChoice.classList.remove('hide')
   showFirstQuestion()
+  setTime()
 }
+
 
 function showFirstQuestion () {
   resetQuiz()
   showQuestion(currentQuestion[firstQuestion]);
 }
+
 
 function showQuestion(questions) {
   questionBox.innerText = questions.question;
@@ -55,7 +67,7 @@ function showQuestion(questions) {
   })
 }
 
-function resetQuiz() {
+function resetQuiz() { //Debug: prev button only shows up on last question
   prevButton.classList.add('hide')
   if (currentQuestion.length == firstQuestion + 1) {
     prevButton.classList.remove('hide')
@@ -66,14 +78,14 @@ function resetQuiz() {
   }
 }
 
-function userAnswer(e) {
-  var clickedAnswer = e.target
+function userAnswer(event) {
+  var clickedAnswer = event.currentTarget
   var correct = clickedAnswer.dataset.correct
   setStatusClass(document.body, correct)
   Array.from(multipleChoice.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
   })
-  if (currentQuestion.length > firstQuestion + 1) {
+  if (currentQuestion.length > firstQuestion + 1 ) {
     nextButton.classList.remove('hide')
   }
   else {
@@ -83,6 +95,8 @@ function userAnswer(e) {
     multipleChoice.classList.add('hide')
   }
 }
+//Debug: last question does not show correct/incorrect class changes
+
 
 function setStatusClass(element, correct) {
   clearStatusClass(element)
@@ -91,13 +105,19 @@ function setStatusClass(element, correct) {
   } else {
     element.classList.add('incorrect')
   }
+  // debug: does not decrease time - where do i put this?
+  // var incorrectAnswerClick = document.querySelector('.incorrect')
+  // incorrectAnswerClick.addEventListener("click", function() {
+  //   secondsLeft - 10      
+  // })
 }
 
 function clearStatusClass(element) {
-  element.classList.add('correct')
-  element.classList.add('incorrect')
+  element.classList.remove('correct')
+  element.classList.remove('incorrect')
 }
 
+//when game is finished, hide buttons, show scoreboard form
 function finishGame() {
   console.log('Finished Game')
   directionsBox.classList.add('hide')
@@ -105,9 +125,7 @@ function finishGame() {
 }
 
 
-//what are the questions?
-//Questions altered from https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
-
+//Define Questions
 var questions = [
   {
     question: '1. Which is the right way to name a function?',
@@ -119,27 +137,100 @@ var questions = [
     ]
   },
    {
-    question: '2. What sound does a cow make',
+    question: '2. Which of the following type of variable is visible only within a function where it is defined?',
     answers: [
-      { text: 'moo', correct: true },
-      { text: 'quack', correct: false },
-      { text: 'meow', correct: false },
-      { text: 'bark', correct: false }
+      { text: 'global variable', correct: false },
+      { text: 'boolean variable', correct: false },
+      { text: 'local variable', correct: true },
+      { text: 'none of the above', correct: false }
+    ]
+  },
+  {
+    question: '3. How would you declare an array',
+    answers: [
+      { text: 'var myArray (1, 2, 3)', correct: false },
+      { text: 'var myArray = ["Apple", "Banana", "Cucumber"]', correct: true },
+      { text: 'var myArray() {"blue", "green", "red"}', correct: false },
+      { text: 'var myArray == 1', correct: false }
     ]
   }
 ]
 
-console.log(questions)
+//Timer
+var timeText = document.querySelector("#time-left")
+var secondsLeft = 180
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--
+    timeText.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      questionBox.classList.add('hide')
+      multipleChoice.classList.add('hide')
+      timeText.textContent = 0
+      clearInterval(timerInterval)
+      finishGame()                                   //if time runs out, finish game
+    } else if (secondsLeft <= 15) {                  //change color for warning
+      timeText.style.color = 'red'
+    }
+  }, 1000)
+
+  //if user finishes game, stop timer and collect score
+  finishButton.addEventListener("click", function() {
+    clearInterval(timerInterval)
+    setScore()
+  })
+
+  // debug: does not decrease timer by ten
+  // var incorrectAnswerClick = document.querySelector('.incorrect')
+  // incorrectAnswerClick.addEventListener("click", function() {
+  //   secondsLeft - 10
+  // }) 
+}
 
 
-
-
-
-
-
-//Start Timer
-
-
-
-
+//debug this doesn't work - how do I get time left into score?
 //Keep Score
+var scoreCount
+
+function setScore() {
+  scoreHere.textContent = timeText
+  localStorage.setItem("scoreCount", scoreHere)
+}
+
+function getScore() {
+  
+  var storedScore = localStorage.getItem("scoreCount")
+  if (storedScore === null) {
+    scoreCounter = 0
+  } else {
+    scoreCounter = storedScore
+  }
+  scoreHere.textContent = scoreCounter
+}
+
+
+//Highscore Board -- didn't have time to finish, this doesn't work
+var scoreboardEl = document.querySelector('#score-board')
+
+function saveHighScore(event) {
+  event.preventDefault()
+  var getInitials = document.querySelector('#initials').value
+  if (!getInitials) {
+    alert("Please enter your initials!")
+  }
+  scoreboardEl.append('<li>' + getInitials + '</li>')
+  getInitials.textContent = ""
+}
+
+scoreboardEl.addEventListener("click", saveHighScore)
+
+
+
+
+
+
+
+
+init()
